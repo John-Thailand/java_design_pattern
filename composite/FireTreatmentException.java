45ccbdf42d78da8e5a52b6338225ee5989a64049
John-Thailand <hamoban69@gmail.com>
2023-03-21 19:41:28 +0900

    compositeパターン

---
+++ composite/FireTreatmentException.java
@@ -0,0 +1,9 @@
+package composite;
+public class FireTreatmentException extends RuntimeException {
+    public FireTreatmentException() {
+
+    }
+    public FireTreatmentException(String msg) {
+        super(msg);
+    }
+}
