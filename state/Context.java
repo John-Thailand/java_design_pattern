9f849b8508a4b9e21393ff3c1fe94e1e781fadae
John-Thailand <hamoban69@gmail.com>
2023-04-05 15:55:17 +0900

    stateパターン

---
+++ state/Context.java
@@ -0,0 +1,7 @@
+package state;
+public interface Context {
+    public abstract void setClock(int hour);
+    public abstract void changeState(State state);
+    public abstract void callSecurityCenter(String msg);
+    public abstract void recordLog(String msg);
+}
